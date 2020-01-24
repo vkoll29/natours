da91631c174e0b221cbcd1b1ea24b693f57c7d3c
vkoll29 <awino_g@outlook.com>
2020-01-24 11:57:11 +0300

    implemented get and post requests

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
@@ -13,9 +14,23 @@ app.get('/api/v1/tours', (req, res) => {
         data: { tours }
     });
 });
-app.post('/', (req, res) => {
-    res.status(200).send("You can post to ths endpoint..")
-})
+
+app.post('/api/v1/tours', (req, res) => {
+    /**my implementation. works */
+    // tours.push(req.body)
+    // fs.writeFileSync(`${__dirname}/dev-data/data/tours-simple2.json`, JSON.stringify(tours));
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTourObj = Object.assign({ id: newId }, req.body);
+    tours.push(newTourObj);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: "success",
+            data: { tour: newTourObj}
+        })
+    });
+
+});
 
 const port = 4000;
 app.listen(port, () => {
