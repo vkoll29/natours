89311e987f72722d9f2dfa3743d00fe46329c535
vkoll29 <awino_g@outlook.com>
2020-01-31 17:41:53 +0300

    added delete and patch verbs but nor implemented. just written

--- app.js
+++ app.js
@@ -15,6 +15,33 @@ app.get('/api/v1/tours', (req, res) => {
     });
 });
 
+app.get('/api/v1/tours/:id/', (req, res) => {
+    tourId = parseInt(req.params.id);
+    /** first solution to a case where the ID doesn't match records*/
+
+    // if(tourId > tours[tours.length - 1].id) return(
+    //   res.status(404).json({
+    //     status: "Failed",
+    //     message: "No such page exists"
+    //   })
+    // )
+
+    /**Second solution*/
+    const tour = tours.find(e => e.id === tourId);
+    if(!tour) return(
+      res.status(404).json({
+        status: "Failed",
+        message: "No such page exists"
+      })
+    )
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour
+        }
+    })
+})
+
 app.post('/api/v1/tours', (req, res) => {
     /**my implementation. works */
     // tours.push(req.body)
@@ -32,8 +59,34 @@ app.post('/api/v1/tours', (req, res) => {
 
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if(parseInt(req.params.id) > tours[tours.length - 1].id) return(
+    res.status(404).json({
+      status: "Failed",
+      message: "Invalid ID passed"
+    })
+  )
+  res.status(201).json({
+    status: "success",
+    message: "Successfully modified the tour with id..."
+  })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) =>{
+  if(parseInt(req.params.id) > tours[tours.length - 1].id) return(
+    res.status(404).json({
+      status: "Failed",
+      message: "Invalid ID passed"
+    })
+  )
+  res.status(204).json({
+    status: "success",
+    data: null
+  })
+})
+
 const port = 4000;
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
-    
+
 })
