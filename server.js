397e07625084e3b6242bdfc655fe1262e3c37469
vkoll29 <awino_g@outlook.com>
2020-02-13 09:25:19 +0300

    - created environment varibales

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
+
 const app = require('./app');
 
-const port = 4000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`server running on port ${port}`);
+    console.log(`server running on port ${port} and NODE_ENV is ${process.env.NODE_ENV}` );
 
 })
