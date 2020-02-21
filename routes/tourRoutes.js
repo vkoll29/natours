f13da0641b1db654802eeb96eb97671e0014c856
vkoll29 <awino_g@outlook.com>
2020-02-21 08:27:39 +0300

    - most of these changes were made by eslint, not that significant

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const router = express.Router();
 const tourController = require('../controllers/tourController');
+
+const router = express.Router();
 
 router.param('id', tourController.checkID);
 
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({path: './config.env'});
 
-const app = require('./app');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`server running on port ${port} and NODE_ENV is ${process.env.NODE_ENV}` );
-
-})
+});
