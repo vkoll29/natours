84f45e5fbb72fc8106807d4ab41d78e011f0a001
vkoll29 <awino_g@outlook.com>
2020-03-02 07:38:03 +0300

    - building an error class

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -25,11 +25,23 @@ app.use(express.static(`${__dirname}/public`));
 // route middlewares
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // err.statusCode = 404;
+  // err.status = 'fail';
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
+  next();
 });
 
 module.exports = app;
