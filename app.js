023b60325e9cf5771d02c1689bbd46085ce6b336
vkoll29 <awino_g@outlook.com>
2020-03-19 17:44:03 +0300

    Added more npm packages to use as middleware for enforcing more security features

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -28,6 +31,26 @@ app.use('/api', limiter);
 
 //Body parser; reading data from body into req.body. also limit the payload
 app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NoSQL injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS attacks
+app.use(xss());
+
+//Preventing parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 
 //serving static fields
 app.use(express.static(`${__dirname}/public`));
