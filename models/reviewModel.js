874335adc6eabe35e417c0ed8d83128991dad709
vkoll29 <awino_g@outlook.com>
2021-01-27 08:01:48 +0300

    created the review schema

---
+++ models/reviewModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'A review must be filled']
+    },
+    rating: {
+      type: Number,
+      max: [5, 'A tour rating should not exceed 5.0'],
+      min: [1, 'A tour name should not be less than 1.0']
+    },
+    createdAt: {
+      type: Date,
+      Default: Date.now()
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'A review must be tied to a tour']
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'A review must be created by an existing user']
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true
+    },
+    toObject: {
+      virtuals: true
+    }
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
