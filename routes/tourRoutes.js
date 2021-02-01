df32a066dba5012022e194a8580a2a501942bfa1
vkoll29 <awino_g@outlook.com>
2021-02-01 14:37:24 +0300

    fixed an authorization issue with createTour

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -34,7 +34,11 @@ router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour
+  );
 router
   .route('/:id')
   .get(tourController.getTour)
